08ddb69ce711f5ddeee75d7e59d374e79b052092
Fatima Angawi <62099300+Fatima-Angawi@users.noreply.github.com>
2026-04-03 15:36:18 +0300

    Update train.py

--- scripts/train.py
+++ scripts/train.py
@@ -2,7 +2,7 @@ import os
 import sys
 from pathlib import Path
 import gc
-ROOT = Path(__file__).resolve().parents[1]
+ROOT = Path("/content/classify_mahalli")
 if str(ROOT) not in sys.path:
     sys.path.insert(0, str(ROOT))
     
